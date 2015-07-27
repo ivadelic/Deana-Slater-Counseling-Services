31c880425fb36c0c853d579f8ee56ced2b101192
Iva Delic <ms.iva.delic@gmail.com>
2015-07-27 01:52:29 -0400

    Added admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def current_admin
-    @current_admin ||= Admin.find(session[:admin_id]) if session[:admin_id]
-  end
+   def current_admin
+     @current_admin ||= Admin.find(session[:admin_id]) if session[:admin_id]
+   end
 
   helper_method :current_admin
 end
