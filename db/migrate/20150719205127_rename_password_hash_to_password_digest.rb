6f1f611a03baeaddaedab455c7dd65fee746976c
Iva Delic <ivad@newyyz.com>
2016-02-17 22:11:23 -0500

    Procfile

--- db/migrate/20150719205127_rename_password_hash_to_password_digest.rb
+++
@@ -1,6 +0,0 @@
-class RenamePasswordHashToPasswordDigest < ActiveRecord::Migration
-  def change
-    add_column :admins, :password_digest, :string
-    remove_column :admins, :pasword_hash, :string
-  end
-end
