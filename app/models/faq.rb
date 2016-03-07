22d876f4b67af48383eee7b256b8d097d905c015
Iva Delic <ivad@newyyz.com>
2016-03-06 23:49:18 -0500

    Fixed destroy/delete bug

--- app/models/faq.rb
+++ app/models/faq.rb
@@ -1,5 +1,6 @@
 class Faq < ActiveRecord::Base
   belongs_to :admin
-  has_many :questions
+  has_many :questions, dependent: :destroy
+  # add_foreign_key :questions, :faqs, on_delete: :cascade
   accepts_nested_attributes_for :questions, :reject_if => :all_blank, :allow_destroy => true
 end
